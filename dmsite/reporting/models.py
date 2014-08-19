cdef5a0e601b200f89591080b2921bbd5d704d39
Delgado <delandre@ua41f7295014e53ee93f7.ant.amazon.com>
2014-08-19 22:50:54 +0000

    added models

--- dmsite/reporting/models.py
+++ dmsite/reporting/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+
+	name = models.CharField(max_length = 45) # name_using_under_score
+	description = models.CharField(max_length = 50) # Pretty Name to display 
+	deleted = models.BooleanField(default = False)
+
+
 class Tables(models.Model):
 
-	pass
-	#category = models.
+	name = models.CharField(max_length = 45) # using_under_score
+	description = models.CharField(max_length = 90) # Name for Display
+	category = models.CharField(max_length = 45) # this matches the name field on the Categories table: uses_dashes
+	deleted = models.BooleanField(default = False)
--- dmsite/reporting/views.py
+++ dmsite/reporting/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from reporting.models import Categories, Tables
 
 # Create your views here.
 
 def index(request):
 
 	#return HttpResponse('this is the index of the DM reporting page')
-	return render_to_response('index.html', )
+	# category_list = [
+	# 					'Prime', 
+	# 					'Store',
+	# 				 	'Library', 
+	# 				 	'ClickStream',
+	# 				  	'TwitterStream'
+	# 				 ] # this will be replaced for a query to the categories table
+	category_list = Categories.objects.all()
+
+	return render_to_response('index.html', {'category_list': category_list})
 
 def report_request(request, category):
 
 	""" use the category to search table 'tables' for for tables under that category to display in drop down menu"""
+	tables = Tables.objects.filter(category= category)
+	
 	display_category = category.replace('_', ' ').title()
-	return render_to_response('report.html', {'category_header' : display_category})
+		
+	return render_to_response('report.html', {'category_header' : display_category, 'tables': tables})
